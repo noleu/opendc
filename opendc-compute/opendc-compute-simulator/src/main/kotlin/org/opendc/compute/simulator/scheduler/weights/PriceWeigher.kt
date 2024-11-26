96f0a7f20d65c285f799169085ca664bc4859a7f
Niels Thiele <noleu66@posteo.net>
2024-11-26 12:15:19 +0100

    added weight determination for price

---
+++ opendc-compute/opendc-compute-simulator/src/main/kotlin/org/opendc/compute/simulator/scheduler/weights/PriceWeigher.kt
@@ -0,0 +1,19 @@
+package org.opendc.compute.simulator.scheduler.weights
+
+import org.opendc.compute.simulator.service.HostView
+import org.opendc.compute.simulator.service.ServiceTask
+
+// TODO: Verify multiplexer logic
+/**
+ * A [HostWeigher] that assigns a weight to a host based on its price.
+ *
+ * @param multiplier The multiplier to apply to the price.  A positive value will result in the scheduler preferring hosts with higher
+ * price, and a negative number will result in the scheduler preferring hosts with a lower price.
+ */
+public class PriceWeigher(override val multiplier: Double = 1.0) : HostWeigher{
+    override fun getWeight(host: HostView, task: ServiceTask): Double {
+        return host.price;
+    }
+
+    override fun toString(): String = javaClass.name;
+}
